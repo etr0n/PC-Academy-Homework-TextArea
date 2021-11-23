e1f285e4660af438fa907049876d32c8c2f76f3f
etr0n <56270962+etr0n@users.noreply.github.com>
2021-11-23 21:57:50 +0200

    Initialcommit

---
+++ script.js
@@ -0,0 +1,46 @@
+const minRows = 10;
+const maxRows = 20;
+const textarea = document.getElementById('textarea');
+textarea.rows = minRows;
+const cols = textarea.cols = 10;
+const min = minRows * textarea.cols;
+const max = maxRows * textarea.cols;
+
+function countEnters (text) {
+  let position = text.indexOf('\n');
+  let count = 0;
+
+  while (position !== -1) {
+    count--;
+    if (!((position % cols) === 0) || position === 0) {
+      count += 10;
+    }
+
+    position = text.indexOf('\n', position + 1);
+  }
+
+  return count;
+}
+
+textarea.addEventListener('input', function () {
+  const length = textarea.value.length;
+  const enters = countEnters(textarea.value);
+  const sum = length + enters;
+
+  if (sum === '') {
+    textarea.rows = minRows;
+    textarea.style.overflowY = 'hidden';
+    console.log('empty');
+  } if (sum > min) {
+    textarea.rows = maxRows;
+    textarea.style.overflowY = 'hidden';
+    console.log('virsijo min rows');
+  } if (sum <= min) {
+    textarea.style.overflowY = 'hidden';
+    textarea.rows = minRows;
+    console.log('sumazinam iki min rows');
+  } if (sum > max) {
+    textarea.style.overflowY = 'scroll';
+    console.log('virsijo max rows');
+  }
+});
